4a500ae16a9a55e06e08e733cc40ca7f348f42e6
Oabraham1 <abrahamojima2018@gmail.com>
2022-10-28 06:44:35 -0400

    Implemented Network and achieved 100% Test coverage

--- network/network.go
+++ network/network.go
@@ -1,19 +1,44 @@
 package network
 
+import (
+	"time"
+)
+
 type Network struct {
-	Nodes 			*[]Node
+	Nodes 			[]*Node
+	Messages 		[]*Message
 	IsNetworkDead 	bool
+}
+
+type Message struct {
+	From 		int64
+	To 			int64
+	Content 	byte
+	Delay 		int64
+	Recieved 	bool
 }
 
 type Node struct {
 	ID 			int64
 	IsNodeDead 	bool
+	Sent 		[]*Message
+	Received 	[]*Message
 }
+
 
 func NewNetwork() *Network {
 	return &Network{
-		Nodes: &[]Node{},
 		IsNetworkDead: false,
+	}
+}
+
+func NewMessage(from, to, delay int64, content byte) *Message {
+	return &Message{
+		From: from,
+		To: to,
+		Content: content,
+		Delay: delay,
+		Recieved: false,
 	}
 }
 
@@ -24,9 +49,91 @@ func NewNode(id int64) *Node {
 	}
 }
 
+func (network *Network) AddMessage(from, to, delay int64, content byte) {
+	message := NewMessage(from, to, delay, content)
+	network.Messages = append(network.Messages, message)
+}
+
+func (network *Network) AddNode(node *Node) {
+	network.Nodes = append(network.Nodes, node)
+}
+
+func (network *Network) GetNode(id int64) *Node {
+	for _, node := range network.Nodes {
+		if node.ID == id {
+			return node
+		}
+	}
+	return nil
+}
+
+func (network *Network) RemoveMessage(message *Message) *Message {
+	for i, m := range network.Messages {
+		if m.From == message.From && m.To == message.To && m.Content == message.Content {
+			network.Messages = append((network.Messages)[:i], (network.Messages)[i+1:]...)
+			return m
+		}
+	}
+	
+	return nil
+}
+
+func (network *Network) RemoveNode(id int64) {
+	for i, n := range network.Nodes {
+		if n.ID == id {
+			network.Nodes = append((network.Nodes)[:i], (network.Nodes)[i+1:]...)
+			n.KillNode()
+		}
+	}
+}
+
+func (network *Network) SendMessage(message *Message) {
+	for _, node := range network.Nodes {
+		if node.ID == message.From && !node.IsNodeDead && node.ID != message.To {
+			node.Sent = append(node.Sent, message)
+			return
+		}
+	}
+}
+
+func (network *Network) ReceiveMessage(message *Message) {
+	for _, node := range network.Nodes {
+		if node.ID == message.To && !node.IsNodeDead {
+			node.Received = append(node.Received, message)
+			message.Recieved = true
+			return
+		}
+	}
+}
+
+func (network *Network) StreamMessages() {
+
+	for len(network.Messages) > int(0) {
+		for _, message := range network.Messages {
+			if message.Delay == 0 {
+				m := network.RemoveMessage(message)
+				if m != nil {
+					network.SendMessage(m)
+					network.ReceiveMessage(m)
+				}
+			}
+			network.Tick(message)
+		}
+	}
+}
+
+func (network *Network) Tick(message *Message) {
+	message.Delay--
+	time.Sleep(time.Second)
+}
+
+func (node *Node) KillNode() {
+	node.IsNodeDead = true
+}
+
 func (network *Network) KillNetwork() {
-	for _, node := range *network.Nodes {
-		node.IsNodeDead = true
+	for _, node := range network.Nodes {
+		node.KillNode()
 	}
 	network.IsNetworkDead = true
 }
--- network/network_test.go
+++ network/network_test.go
@@ -1,6 +1,7 @@
 package network
 
 import (
+	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -13,6 +14,130 @@ func CreateNetwork(t *testing.T) (*Network){
 
 func TestCreateNetWork(t *testing.T) {
 	network := CreateNetwork(t)
-	require.Equal(t, len(*network.Nodes), int(0))
+	require.Equal(t, len(network.Nodes), int(0))
 	require.Equal(t, network.IsNetworkDead, false)
+}
+
+func TestAddNode(t *testing.T) {
+	network := CreateNetwork(t)
+	for i := 0; i < 5; i++ {
+		network.AddNode(NewNode(int64(i)))
+	}
+	require.Equal(t, len(network.Nodes), int(5))
+}
+
+func TestRemoveNode(t *testing.T) {
+	network := CreateNetwork(t)
+	for i := 0; i < 5; i++ {
+		network.AddNode(NewNode(int64(i)))
+	}
+	for i := 0; i < 5; i++ {
+		network.RemoveNode(int64(i))
+	}
+	require.Equal(t, len(network.Nodes), int(0))
+}
+
+func TestRemoveMessage(t *testing.T) {
+	network := CreateNetwork(t)
+	for i := 0; i < 5; i++ {
+		network.AddNode(NewNode(int64(i)))
+	}
+	require.Equal(t, len(network.Nodes), int(5))
+
+	for i := 0; i < 5; i++ {
+		network.AddMessage(int64(i), int64(i+1), 4, byte(i))
+	}
+	require.Equal(t, len(network.Messages), int(5))
+
+	for i := 0; i < 5; i++ {
+		message := network.RemoveMessage(NewMessage(int64(i), int64(i+1), rand.Int63n(150), byte(i)))
+		require.NotNil(t, message)
+	}
+	require.Equal(t, len(network.Messages), int(0))
+	message := network.RemoveMessage(NewMessage(int64(100), int64(100), rand.Int63n(150), byte(100)))
+	require.Nil(t, message)
+}
+
+func TestSendAndRecieveMessage(t *testing.T) {
+	network := CreateNetwork(t)
+	for i := 0; i < 5; i++ {
+		network.AddNode(NewNode(int64(i)))
+	}
+	require.Equal(t, len(network.Nodes), int(5))
+
+	for i := 0; i < 5; i++ {
+		network.AddMessage(int64(i), int64(i+1), 4, byte(i))
+	}
+	require.Equal(t, len(network.Messages), int(5))
+
+	for i := 0; i < 4; i++ {
+		message := network.Messages[i]
+		network.SendMessage(message)
+		require.Equal(t, message.Recieved, false)
+		require.Equal(t, len(network.GetNode(message.From).Sent), int(1))
+		require.Nil(t, network.GetNode(100))
+
+		network.ReceiveMessage(message)
+		require.Equal(t, message.Recieved, true)
+		require.Equal(t, len(network.GetNode(message.To).Received), int(1))
+	}
+}
+
+func TestStreamMessages(t *testing.T) {
+	network := CreateNetwork(t)
+	for i := 0; i < 4; i++ {
+		network.AddNode(NewNode(int64(i)))
+	}
+	require.Equal(t, len(network.Nodes), int(4))
+
+	network.AddMessage(int64(0), int64(1), rand.Int63n(3), byte(0))
+	network.AddMessage(int64(1), int64(0), rand.Int63n(3), byte(1))
+	require.Equal(t, len(network.Messages), int(2))
+
+	network.StreamMessages()
+
+	require.Equal(t, len(network.Messages), int(0))
+	require.Equal(t, len(network.GetNode(int64(0)).Sent), int(1))
+	require.Equal(t, len(network.GetNode(int64(1)).Sent), int(1))
+	require.Equal(t, len(network.GetNode(int64(0)).Received), int(1))
+	require.Equal(t, len(network.GetNode(int64(1)).Received), int(1))
+	require.Equal(t, network.GetNode(int64(1)).Sent[0].Delay, int64(-1))
+}
+
+func TestTick(t *testing.T) {
+	network := CreateNetwork(t)
+	for i := 0; i < 5; i++ {
+		network.AddNode(NewNode(int64(i)))
+	}
+	require.Equal(t, len(network.Nodes), int(5))
+
+	for i := 0; i < 5; i++ {
+		network.AddMessage(int64(i), int64(i+1), 4, byte(i))
+	}
+	require.Equal(t, len(network.Messages), int(5))
+
+	messages := network.Messages
+	for _, message := range network.Messages {
+		delay := message.Delay
+		network.Tick(message)
+		require.Equal(t, message.Delay, delay-1)
+	}
+
+	for i, message := range network.Messages {
+		require.Equal(t, message.Delay, (messages)[i].Delay)
+	}
+}
+
+func TestKillNetwork(t *testing.T) {
+	network := CreateNetwork(t)
+	for i := 0; i < 5; i++ {
+		network.AddNode(NewNode(int64(i)))
+	}
+	require.Equal(t, len(network.Nodes), int(5))
+	network.KillNetwork()
+
+	for i := 0; i < 5; i++ {
+		require.Equal(t, network.GetNode(int64(i)).IsNodeDead, true)
+	}
+	require.Equal(t, network.IsNetworkDead, true)
 }
